273afe3d58292c10f45de0df011550b722e176df
Farai <farai.motsi@gmail.com>
2019-11-05 08:43:42 +0200

    fixed html template

--- js/app.js
+++ js/app.js
@@ -15,22 +15,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
             //    articles.map(article=>{  })
             articles.forEach(article => {
                 HTMLTemplate += `
-                    <div class="col-md-4 mt-4">
-                         <div class="card">
-                              <div class="card-body">
-                                   <h2>${article.title}</h2> 
-                                   <h2>${article.publishedAt}</h2> 
-                              </div>
-                              <div class="card-body">
-                              <p>${article.description}</p> 
-                              <img src="${article.urlToImage}"/>
-                         </div>
+                    
+   <div class="row">
+   <div class="col-sm-3"><img src="${article.urlToImage}"/ width="20%"></div> 
+   <div class="col-sm-6">${article.publishedAt} ${article.title}</div> 
+   <div class="col-sm-3">${article.description}></div>                         
                              
-                         </div>
                     </div>
                
                `;
             });
+            const results = document.querySelector('#results');
             this.results.innerHTML = HTMLTemplate;
         });
 });
